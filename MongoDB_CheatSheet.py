92cbaa5cfdd50fbed299ecd1ec479322f20be378
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-05-11 22:35:19 -0700

    Update MongoDB_CheatSheet.py

--- MongoDB_CheatSheet.py
+++ MongoDB_CheatSheet.py
@@ -20,5 +20,21 @@ db.users.find()   # show everything in database
                    
 #How To Read Data From Database  
 db.users.find().limit(2)  #show 2 things in databse
-db.users.find().sort({name: 1}).limit(2)  #ascending order limit by 2 [A-Z]
-db.users.find().sort({name: -1}).limit(2) #descending order linit by 2 [Z-A]
+db.users.find().sort({name: 1}).limit(2)  #sort ascending order limit by 2 [A-Z]
+db.users.find().sort({name: -1}).limit(2) #sort descending order linit by 2 [Z-A]
+db.users.find().sort({age: 1, name: 1}) #sort multipoe fields (age & name)
+db.users.find().skip(1).  #skip 1st entry
+
+#Queries
+db.users.find({name: "Sally"})  #returns query where object is "Sally"
+db.users.find({name: "Sally"},{address: 0}) #retuns multiple queries (NOTE - 0 will return NOTHING for that field)
+db.users.find({name: "Sally"},{address: 1}) #retuns multiple queries (NOTE - 1 will return EVERYTHING for that field)
+
+#Complex Queries
+db.users.find({ name: { $eq: "Sally"}}) #returns query where name Equals "Sally"
+db.users.find({ name: { $ne: "Sally"}}) #returns query where name does NOT EQUAL "Sally"
+db.users.find({ age: { $gt: 13}}) #returns query where age > 13
+db.users.find({ age: { $gte: 46}})  #returns query where age is >= 46
+
+                   
+
