ca56e7a2b68f2c4df3b40c8d1b4e91481e6432b1
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-05-18 23:20:51 -0700

    Update SQL_exercises.py

--- SQL_exercises.py
+++ SQL_exercises.py
@@ -32,3 +32,17 @@ AND PostalCode = 12209;
 SELECT * FROM Customers
 WHERE City = 'Berlin'
 OR City = 'London';
+
+#----------- SQL Order By -------------
+# Select all records from the Customers table, sort the result alphabetically by the column City.
+SELECT * FROM Customers
+ORDER BY City;
+
+# Select all records from the Customers table, sort the result reversed alphabetically by the column City.
+SELECT * FROM Customers
+Order By City Desc;
+
+# Select all records from the Customers table, sort the result alphabetically, first by the column Country, then, by the column City.
+SELECT * FROM Customers
+ORDER BY Country, City;
+
