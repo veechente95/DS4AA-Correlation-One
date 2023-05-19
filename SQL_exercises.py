5997c3d5e418d7cae4a2e8a5b7f82e55fcd3ba13
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-05-19 13:20:52 -0700

    Update SQL_exercises.py

--- SQL_exercises.py
+++ SQL_exercises.py
@@ -114,6 +114,40 @@ WHERE Price = 18;
 SELECT AVG(Price)
 FROM Products;
 
+# Use an SQL function to calculate the sum of all the Price column values in the Products table.
+SELECT SUM(Price)
+FROM Products;
+
+#----------- SQL Like -------------
+# Select all records where the value of the City column starts with the letter "a".
+SELECT * FROM Customers
+WHERE City LIKE 'a%';
+
+# Select all records where the value of the City column ends with the letter "a".
+SELECT * FROM Customers
+WHERE City LIKE '%a';
+
+# Select all records where the value of the City column contains the letter "a".
+SELECT * FROM Customers
+WHERE City LIKE '%a%';
+
+# Select all records where the value of the City column starts with letter "a" and ends with the letter "b".
+SELECT * FROM Customers
+WHERE City LIKE 'a%b';
+
+# Select all records where the value of the City column does NOT start with the letter "a".
+SELECT * FROM Customers
+WHERE City NOT LIKE 'a%';
+
+#----------- SQL Wildcards -------------
+
+
+
+
+
+
+
+
 
 
 
