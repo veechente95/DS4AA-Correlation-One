1a743358d13a693b357dc0018945c6431b4b9ee8
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-05-29 13:41:08 -0700

    Update pandas_practice.py

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,5 +1,21 @@
+#Pandas Intro
 import pandas as pd   #import poandas
 df = pd.read_csv("FileName.csv")   #read dataframe
+
+df.head() #inspect first 5 rows
+df.tail() #inspect last 5 rows 
+df["column"]  #look at whole column
+
+# By default, pandas assigns a serial number as the index (that's the unnamed column of integers in bold face in the df.head() 
+# output above), so setting a custom row index that makes sense for your DataFrame is often a good idea. 
+# You can set the index of a DataFrame with the .set_index() method. Here is an example: 
+df = df.set_index(["model", "store", "condition"])
+df.head()
+
+groups = df.groupby(["column1", "column2"]) # Grouping by
+groups["column1"].mean() # You first slice using ["column1"] and then call the mean function on the grouped Series
+
+#Pandas Methods
 df["column"].drop_dupicates()  # drops duplicates
 df["column"].drop_duplicates().to_list()  #shows all categories w/o/ duplicates in list form
 df["column"].min() #gets min value
