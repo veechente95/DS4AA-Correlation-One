a54272f69871e8f3b4b6b33b60087e32814bc106
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-06-02 18:05:27 -0700

    Update PostgreSQL_Exercises.sql

--- PostgreSQL_Exercises.sql
+++ PostgreSQL_Exercises.sql
@@ -63,9 +63,30 @@ where joindate =
 	(select max(joindate)
 		from cd.members);
 
+--Retrieve the start times of members' bookings
+--How can you produce a list of the start times for bookings by members named 'David Farrell'?
+select bks.starttime
+	from
+		cd.bookings bks
+		inner join cd.members mems
+			on mems.memid = bks.memid
+	where 
+		mems.firstname='David'
+		and mems.surname='Farrell';
 
-
-
+--Work out the start times of bookings for tennis courts
+--How can you produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'? 
+--Return a list of start time and facility name pairings, ordered by the time.
+select bks.starttime as start, facs.name as name
+	from
+		cd.facilities facs
+		inner join cd.bookings bks
+			on facs.facid = bks.facid
+	where
+		facs.name in ('Tennis Court 2', 'Tennis Court 1') and
+		bks.starttime >= '2012-09-21' and
+		bks.starttime < '2012-09-22'
+order by bks.starttime;
 
 
 
