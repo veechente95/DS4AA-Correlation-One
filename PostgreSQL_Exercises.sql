7a7efe62d271f787a95e260dc3aa8cff39493b7d
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-06-06 20:18:29 -0600

    Update PostgreSQL_Exercises.sql

--- PostgreSQL_Exercises.sql
+++ PostgreSQL_Exercises.sql
@@ -88,5 +88,13 @@ select bks.starttime as start, facs.name as name
 		bks.starttime < '2012-09-22'
 order by bks.starttime;
 
+--Produce a list of all members who have recommended another member
+--How can you output a list of all members who have recommended another member? 
+--Ensure that there are no duplicates in the list, and that results are ordered by (surname, firstname).
+select distinct recs.firstname as firstname, recs.surname as surname
+from cd.members mems
+inner join cd.members recs
+on recs.memid = mems.recommendedby
+order by surname, firstname;
 
 
