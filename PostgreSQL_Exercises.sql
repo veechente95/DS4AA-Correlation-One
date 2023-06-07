16287a371ddba05aba63baf9d0fa99f9d18cc2a0
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-06-06 20:27:29 -0600

    Update PostgreSQL_Exercises.sql

--- PostgreSQL_Exercises.sql
+++ PostgreSQL_Exercises.sql
@@ -97,4 +97,12 @@ inner join cd.members recs
 on recs.memid = mems.recommendedby
 order by surname, firstname;
 
-
+--Produce a list of all members, along with their recommender
+--How can you output a list of all members, including the individual who recommended them (if any)? 
+--Ensure that results are ordered by (surname, firstname).
+select mems.firstname as memfname, mems.surname as memsname, recs.firstname as recfname, recs.surname as recsname
+	from 
+		cd.members mems
+		left outer join cd.members recs
+			on recs.memid = mems.recommendedby
+order by memsname, memfname;
