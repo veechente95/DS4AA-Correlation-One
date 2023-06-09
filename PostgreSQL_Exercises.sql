ec5cfd1f3dfff42acbe231952e0c96201249d978
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-06-09 11:29:10 -0600

    Update PostgreSQL_Exercises.sql

--- PostgreSQL_Exercises.sql
+++ PostgreSQL_Exercises.sql
@@ -122,3 +122,31 @@ select distinct mems.firstname || ' ' || mems.surname as member, facs.name as fa
 		facs.name in ('Tennis Court 2', 'Tennis Court 1')
 order by member, facility
 
+--Produce a list of costly bookings
+--How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? 
+--Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. 
+--Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. 
+--Order by descending cost, and do not use any subqueries
+select mems.firstname || ' ' || mems.surname as member,
+	facs.name as facility,
+	case
+		when mems.memid = 0 then
+			bks.slots*facs.guestcost
+		else
+			bks.slots*facs.membercost
+	end as cost
+	from
+		cd.members mems
+		inner join cd.bookings bks
+			on mems.memid = bks.memid
+		inner join cd.facilities facs
+			on bks.facid = facs.facid
+	where 
+		bks.starttime >= '2012-09-14' and
+		bks.starttime < '2012-09-15' and (
+			(mems.memid = 0 and bks.slots*facs.guestcost > 30) or
+			(mems.memid != 0 and bks.slots*facs.membercost > 30)
+		)
+order by cost desc;
+		
+
