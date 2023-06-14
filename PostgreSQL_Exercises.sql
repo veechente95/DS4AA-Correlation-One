4b795c70cd2e2f6153e738b3a76811c518ff97b3
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-06-13 19:27:14 -0700

    Update PostgreSQL_Exercises.sql

--- PostgreSQL_Exercises.sql
+++ PostgreSQL_Exercises.sql
@@ -149,4 +149,15 @@ select mems.firstname || ' ' || mems.surname as member,
 		)
 order by cost desc;
 		
+--Produce a list of all members, along with their recommender, using no joins.
+--How can you output a list of all members, including the individual who recommended them (if any), without using any joins? 
+--Ensure that there are no duplicates in the list, and that each firstname + surname pairing is formatted as a column and ordered.
+select distinct mems.firstname || ' ' ||  mems.surname as member,
+	(select recs.firstname || ' ' || recs.surname as recommender 
+		from cd.members recs 
+		where recs.memid = mems.recommendedby
+	)
+	from 
+		cd.members mems
+order by member;   
 
